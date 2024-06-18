01df77270abcd8169f6a107bf2a2674fc18376e0
Brian Rayburn <bgrayburn@gmail.com>
2024-06-18 11:54:24 -0400

    even more minimal

--- component/devenv.nix
+++ component/devenv.nix
@@ -1,8 +1,5 @@
 { pkgs, lib, config, inputs, ... }:
 
 {
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
   processes.component.exec = "echo $DEVENV_ROOT && $DEVENV_ROOT/src/test.sh && sleep 2";
 }
